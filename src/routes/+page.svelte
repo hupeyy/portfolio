8f860297338f2bf2c43fe118ee8da450718cc67f
hupeyy <huperw@ufl.edu>
2025-02-16 18:20:52 -0500

    add threejs later

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
 <script>
-    import App from '../lib/components/App.svelte';
+    import Typewriter from 'svelte-typewriter';
+    const messages = ["web developer", "sudoku solver", "baker"];
 </script>
 
-<App />
+<div class="flex flex-row text-lg gap-1">
+    <div>Hi, I'm a...</div>
+    <Typewriter
+        keepCursorOnFinish={true}
+        mode={'loop'}
+        delay={100}
+        interval={100}
+        unwriteInterval={100}
+    >
+        {#each messages as message}
+            <div>{message}</div>
+        {/each}
+    </Typewriter>
+</div>
